102520de8aafb52bd08331fa8b789c477b710117
Suraj Gupta <surajgupta7070031833@gmail.com>
2026-05-10 18:51:47 +0530

    Implement data validation functionality with schema checks and dataset drift detection; add DataValidation class and related constants

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import logging
 import sys
 
+from networksecurity import logging
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.entity.config_entity import (
     DataIngestionConfig,
+    DataValidationConfig,
     TrainingPipelineConfig,
 )
 from networksecurity.exception.exception import NetworkSecurityException
@@ -20,5 +22,14 @@ if __name__ == "__main__":
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data ingestion completed successfully")
         print(data_ingestion_artifact)
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(
+            data_ingestion_artifact=data_ingestion_artifact,
+            data_validation_config=data_validation_config,
+        )
+        logging.info("Starting data validation")
+        data_validation_artifact = data_validation.initialize_data_validation()
+        logging.info("Data validation completed successfully")
+        print(data_validation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
